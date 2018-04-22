f653a6bb7d31e056f4d13d061b381b0b7077bd14
Ema <baldosa@users.noreply.github.com>
2018-04-22 12:54:06 -0300

    2nd commit

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,64 +1,45 @@
 #include "SevSeg.h"
 #include <Wire.h>
 #include "RTClib.h"
+#include "EEPROM.h"
+#include "EEPROMAnything.h"
 
 SevSeg sevseg; 
 RTC_DS3231 rtc;
 int buttonPin = A1;
-int mark = 0;
-/*    
- 7-seg 4 digit model A-564SR
-     A
-   *****
- F *   *
-   * G * B
-   *****
- E *   * 
-   *   * C
-   *****
-     D
-  
-  A -> 11
-  B -> 7
-  C -> 4
-  D -> 2
-  E -> 1
-  F -> 10
-  G -> 5
-  P -> 3
-  
-  
-  1 -> 12
-  2 -> 9
-  3 -> 8
-  4 -> 6
+DateTime markTime;
+TimeSpan recordTime;
+TimeSpan showTime;
+float pressLength = 0;
+float showLength = 1000;
 
-  digitPins from 1 to 4
-  segmentPins from A to P
-  */
- 
 void setup(){
   //serial for debugging
-  Serial.begin(9600);
+  //Serial.begin(9600);
 
   // sevseg vars //
-  byte numDigits = 4;
+  byte numDigishowTime = 4;
   byte digitPins[] = {10, 11, 12, 13};
   byte segmentPins[] = {6, 5, 2, 3, 4, 7, 8, 9};
-  bool resistorsOnSegments = true; 
+  bool resistorsOnSegmenshowTime = true; 
   bool updateWithDelaysIn = true;
   byte hardwareConfig = COMMON_ANODE; 
-  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
+  sevseg.begin(hardwareConfig, numDigishowTime, digitPins, segmentPins, resistorsOnSegmenshowTime);
   sevseg.setBrightness(90);
 
   // rtc shit //
   rtc.begin();
-  //if you lose the pila you set the time
+  //if you lose the pila sets the time
   //yyyy, m, d, hh, mm, ss
-  rtc.adjust(DateTime(2017, 4, 19, 00, 54, 0));
+  //rtc.adjust(DateTime(2018, 4, 21, 13, 40, 30));
 
   //buton
   pinMode(buttonPin, INPUT_PULLUP);
+
+  
+  //EEPROM things
+  EEPROM_readAnything(0, markTime);
+  EEPROM_readAnything(1, recordTime);
 
 
 }
@@ -66,24 +47,65 @@ void setup(){
 void loop(){
     //the time is now
     DateTime now = rtc.now();
+
+    showTime = now - markTime;
+
+    //testing
+      //Serial.print("start");
+      //Serial.println();
+      //Serial.print(showTime.hours(), DEC);
+      //Serial.print(":");
+      //Serial.print(showTime.minutes(), DEC);
+      //Serial.print(":");
+      //Serial.print(showTime.seconds(), DEC);
+      //Serial.println();
+      //Serial.print(markTime.hour(), DEC);
+      //Serial.print(':');
+      //Serial.print(markTime.minute(), DEC);
+      //Serial.print(':');
+      //Serial.print(markTime.second(), DEC);
+      //Serial.println();
+      //Serial.print(now.hour(), DEC);
+      //Serial.print(':');
+      //Serial.print(now.minute(), DEC);
+      //Serial.print(':');
+      //Serial.print(now.second(), DEC);
+      //Serial.println();
+      //Serial.print(recordTime.hours(), DEC);
+      //Serial.print(':');
+      //Serial.print(recordTime.minutes(), DEC);
+      //Serial.print(':');
+      //Serial.print(recordTime.seconds(), DEC);
+      //Serial.println();
+      //Serial.println(boolString[now < markTime]);
+      //sevseg.setNumber(100*showTime.days()+showTime.hours(),4);
+    sevseg.setNumber(100*showTime.hours()+showTime.minutes(),4);
+
+    sevseg.refreshDisplay();
         
-    //if button push log that else display now-mark
+    //meassure press button length
+    //while (digitalRead(buttonPin) == LOW ){
+    //  delay(100);
+    //  pressLength = pressLength + 100;
+    //}
+    
+    //if button push log that
+    //if (pressLength < showLength) {
     if (digitalRead(buttonPin) == LOW) {
-      mark = now.second();
-    }
+      markTime =  rtc.now();
+      EEPROM_writeAnything(0, markTime);
+
+    //if showTime is the new record, save that
+      if (showTime > recordTime) {
+    	EEPROM_writeAnything(1, recordTime);
+      }
+     }
+
+    //if (pressLength > showLength) {
+    //   sevseg.setNumber(100*recordTime.hours()+recordTime.minutes(),4);
+    // }
     
-    else {
-      //sevseg.setNumber(100*(now.second()-mark)+mark,4);
-      
-      //testing
-      Serial.print(now.second()-mark, DEC);
-      Serial.print(' ');
-      Serial.print(mark, DEC);
-      Serial.println();
-    }
-    delay(1000);
-    
-     //sevseg.refreshDisplay();
+    //delay(1000);
     
 }
 
